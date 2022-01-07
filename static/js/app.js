bcaf7eda79bb952173765a28afec728bc4f6b030
matin-n <40713017+matin-n@users.noreply.github.com>
2022-01-07 15:43:02 -0800

    Filter UFO sightings on multiple criteria

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-// import the data from data.js
+// Import the data from data.js
 const tableData = data;
 
 // Reference the HTML table using d3
@@ -23,23 +23,56 @@ function buildTable(data) {
   });
 }
 
-function handleClick() {
-  // Grab the datetime value from the user defined filter
-  let date = d3.select("#datetime").property("value");
+// 1. Create a variable to keep track of all the filters as an object.
+var filters = {};
+
+// 3. Use this function to update the filters.
+function updateFilters() {
+  // 4a. Save the element that was changed as a variable.
+  let changedElement = d3.select(this);
+
+  // 4b. Save the value that was changed as a variable.
+  let elementValue = changedElement.property("value");
+  console.log(elementValue);
+
+  // 4c. Save the id of the filter that was changed as a variable.
+  let filterId = changedElement.attr("id");
+  console.log(filterId);
+
+  // 5. If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object.
+  if (elementValue) {
+    filters[filterId] = elementValue;
+  } else {
+    delete filters[filterId];
+  }
+  // console.log(filters);
+
+  // 6. Call function to apply all filters and rebuild the table
+  filterTable();
+}
+
+// 7. Use this function to filter the table when data is entered.
+function filterTable() {
+  // 8. Set the filtered data to the tableData.
   let filteredData = tableData;
 
-  // If a date was entered then, filter the data using the specified date
-  if (date) {
-    filteredData = filteredData.filter((row) => row.datetime === date);
+  // 9. Loop through all the filters and keep any data that matches the filter values
+
+  // Destructuring for...in is deprecated. Use for...of instead.
+  // SOURCE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Deprecated_and_obsolete_features#statements
+  // The Object.entries() method returns an array of a given object's own enumerable string-keyed property [key, value] pairs
+  // SOURCE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+  for (const [key, value] of Object.entries(filters)) {
+    filteredData = filteredData.filter((row) => row[key] === value);
   }
 
-  // Rebuild the table using the filtered data
-  // The original table will be shown if no date was specified
+  // 10. Rebuild the table using the filtered data
   buildTable(filteredData);
 }
 
-// Attach handleClick() on click event of filter button
-d3.selectAll("#filter-btn").on("click", handleClick);
+// 2. Attach an event to listen for changes to each filter
+d3.selectAll("input").on("change", updateFilters);
 
 // Build the table when the page loads
 buildTable(tableData);
